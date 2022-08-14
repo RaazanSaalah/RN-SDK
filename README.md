f126f61ed2c995a92f0e2b6a8168d9b4ac5d8c50
RaazanSaalah <110047759+RaazanSaalah@users.noreply.github.com>
2022-08-14 16:17:14 +0200

    Initial SDK version

---
+++ README.md
@@ -0,0 +1,25 @@
+# mytestpackagerr
+
+- npm Package return button with show modal on click event
+- User send a Required Parameters "PublicKey , ClientSecret"
+
+## How to use it
+
+1 - Install in your Project using this command
+
+```sh
+npm i mytestpackagerr
+```
+
+2 - Import it in your RN Project
+
+```sh
+import Test from "mytestpackagerr";
+```
+
+3- Call it with your own parameters
+
+```sh
+ <Test publicKey="PK_4545154545" clientSecret="cS_34" />
+```
+# RN-SDK
---
+++ index.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Pressable, Text, View, Alert, Modal, StyleSheet } from "react-native";
+
+export default function Test(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Pressable onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.exit}>X</Text>
+            </Pressable>
+            <Text style={styles.modalText}>Piblic:{props.publicKey}</Text>
+            <Text style={styles.modalText}>
+              clientSecret:{props.clientSecret}
+            </Text>
+          </View>
+        </View>
+      </Modal>
+      <Pressable style={styles.button} onPress={() => setModalVisible(true)}>
+        <Text style={styles.text}>Checkout Now</Text>
+      </Pressable>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 82,
+    margin: 40,
+    borderRadius: 5,
+    backgroundColor: "#0180FD",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "medium",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  modalView: {
+    height: "100%",
+    backgroundColor: "white",
+    padding: 32,
+    shadowColor: "#000",
+  },
+
+  exit: {
+    color: "grey",
+    fontSize: 31,
+    textAlign: "right",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
